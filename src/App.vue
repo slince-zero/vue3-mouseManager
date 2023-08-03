05b367c1b6c945eb2f3a2ce2b8036736e4d0bd41
slince-zero <1711257704@qq.com>
2023-08-03 17:46:25 +0800

    fix:API请求接口统一管理

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,12 @@
 </template>
 
 <script setup lang="ts">
-import request from '@/utils/request'
+import{reqLogin} from './api/user'
 import { onMounted } from 'vue'
 
 onMounted(()=>{
-  request({
-    url:'/user/login',
-    method:'post',
-    data:{
-      username:'admin',
-      password:'111111'
-    }
-  }).then(res=>{
-    console.log(res);
+  reqLogin({username:'admin',password:'111111'}).then((req)=>{
+    console.log(req);
     
   })
 })
--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,17 +1,17 @@
 // 统一管理项目中用户相关的接口
 
 import request from '@/utils/request'
+import type { loginForm, loginResponseData, userResponseDara } from './type'
 
 enum API {
-  LOGIN_URL = '/admin/acl/index/login',
-
-  USERINFO_URL = '/admin/acl/index/info',
-
-  LOGOUT_URL = '/admin/acl/index/logout',
+  LOGIN_URL = '/user/login',
+  USERINFO_URL = '/user/info'
 }
 
 // 登录接口方法
-export const reqLogin =(data:any)=>request.post(API.LOGIN_URL,data)
+export const reqLogin = (data: loginForm) =>
+  request.post<any, loginResponseData>(API.LOGIN_URL, data)
 
 // 获取用户信息接口方法
-export const reqUserInfo =()=> request.get(API.USERINFO_URL)
+export const reqUserInfo = () =>
+  request.get<any, userResponseDara>(API.USERINFO_URL)

fafe8c5b526aea8ba8139d676b3a5d11a1227b2f
slince-zero <1711257704@qq.com>
2023-08-13 20:59:11 +0800

    fix:bug

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,13 +1,29 @@
 // 品牌管理模块接口
 import request from '@/utils/request'
 // 引入接口类型
-import {TradeMarkResponseData} from './type'
+import { TradeMarkResponseData, TradeMark } from './type'
 enum API {
   // 获取已有品牌接口
   TRADEMARK_URL = '/admin/product/baseTrademark/',
+  // 添加品牌
+  ADDTRADEMARK_URL = '/admin/product/baseTrademark/save',
+  // 修改品牌
+  UPDATETRADEMARK_URL = '/admin/product/baseTrademark/update',
 }
 
 // page：获取第几页，默认第一页
 // limit：获取几个已有品牌的数据
 export const reqHasTrademark = (page: number, limit: number) =>
-  request.get<any, TradeMarkResponseData>(API.TRADEMARK_URL + `${page}/${limit}`)
+  request.get<any, TradeMarkResponseData>(
+    API.TRADEMARK_URL + `${page}/${limit}`,
+  )
+
+// 添加和修改
+export const reqAddOrUpdateTradeMark = (data: TradeMark) => {
+  // 修改
+  if (data.id) {
+    return request.put<any, any>(API.UPDATETRADEMARK_URL, data)
+  } else {
+    return request.post<any, any>(API.ADDTRADEMARK_URL)
+  }
+}
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,53 +1,103 @@
 <template>
-  <el-card class="box-card">
-    <!-- 添加品牌按钮 -->
-    <el-button type="primary" icon="Plus">添加品牌</el-button>
-    <!-- 表格组件，展示品牌数据 -->
-    <el-table style="margin: 10px 0" border :data="trademarkArr">
-      <el-table-column
-        label="序号"
-        align="center"
-        type="index"
-      ></el-table-column>
-      <el-table-column
-        label="品牌名称"
-        align="center"
-        prop="tmName"
-      ></el-table-column>
-      <el-table-column label="品牌LOGO" align="center">
-        <template #="{ row, $index }">
-          <img
-            :src="row.logoUrl"
-            alt="找不到图片"
-            style="width: 100px; height: 100px"
-          />
-        </template>
-      </el-table-column>
-      <el-table-column label="品牌操作" align="center">
-        <template #="{ row, $index }">
-          <el-button type="primary" size="small" icon="Edit"></el-button>
-          <el-button type="primary" size="small" icon="Delete"></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+  <div>
+    <!-- 品牌展示 -->
+    <el-card class="box-card">
+      <!-- 添加品牌按钮 -->
+      <el-button type="primary" icon="Plus" @click="addTradeMark">
+        添加品牌
+      </el-button>
+      <!-- 表格组件，展示品牌数据 -->
+      <el-table style="margin: 10px 0" border :data="trademarkArr">
+        <el-table-column
+          label="序号"
+          align="center"
+          type="index"
+        ></el-table-column>
+        <el-table-column
+          label="品牌名称"
+          align="center"
+          prop="tmName"
+        ></el-table-column>
+        <el-table-column label="品牌LOGO" align="center">
+          <template #="{ row, $index }">
+            <img
+              :src="row.logoUrl"
+              alt="找不到图片"
+              style="width: 100px; height: 100px"
+            />
+          </template>
+        </el-table-column>
+        <el-table-column label="品牌操作" align="center">
+          <template #="{ row, $index }">
+            <el-button
+              type="primary"
+              size="small"
+              icon="Edit"
+              @click="updateTradeMark"
+            ></el-button>
+            <el-button
+              type="primary"
+              size="small"
+              icon="Delete"
+              @click="deleteTradeMark"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
 
-    <!-- 分页 -->
-    <el-pagination
-      v-model:current-page="pageNo"
-      v-model:page-size="limit"
-      :page-sizes="[3, 5, 7, 9]"
-      :background="true"
-      layout=" prev, pager, next,jumper,->,sizes,total"
-      :total="total"
-      @click="changePageNo"
-      @size-change="sizeChange"
-    />
-  </el-card>
+      <!-- 分页 -->
+      <el-pagination
+        v-model:current-page="pageNo"
+        v-model:page-size="limit"
+        :page-sizes="[3, 5, 7, 9]"
+        :background="true"
+        layout=" prev, pager, next,jumper,->,sizes,total"
+        :total="total"
+        @click="changePageNo"
+        @size-change="sizeChange"
+      />
+    </el-card>
+    <!-- 新增品牌 -->
+    <el-dialog v-model="dialogFormVisible" title="添加品牌">
+      <el-form style="width: 80%">
+        <el-form-item label="品牌名称">
+          <el-input
+            placeholder="请输入品牌名称"
+            label-width="80px"
+            v-model="tradeMarkParams.tmName"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="80px">
+          <el-upload
+            class="avatar-uploader"
+            action="http://139.198.104.58:8209/admin/product/fileUpload"
+            :show-file-list="false"
+          >
+            <img
+              v-if="tradeMarkParams.logoUrl"
+              :src="tradeMarkParams.logoUrl"
+              class="avatar"
+            />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button @click="cancle">取消</el-button>
+        <el-button @click="submit">提交</el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { reqHasTrademark } from '@/api/product/trademark'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, reactive } from 'vue'
+import {
+  Records,
+  TradeMarkResponseData,
+  TradeMark,
+} from '@/api/product/trademark/type'
 // 当前页码
 const pageNo = ref<number>(1)
 // 每一页展示多少条数据
@@ -56,8 +106,10 @@ const limit = ref<number>(3)
 // 定义已有品牌数据数量
 const total = ref<number>(0)
 
+// 控制新增页面的显示与隐藏变量
+const dialogFormVisible = ref<boolean>(false)
 // 引入品牌数据类型
-import { Records, TradeMarkResponseData } from '@/api/product/trademark/type'
+
 // 定义要展示的品牌数据
 const trademarkArr = ref<Records>([])
 // 获取品牌接口
@@ -72,6 +124,12 @@ const getHasTrademark = async () => {
   }
 }
 
+// 定义收集数据的对象
+const tradeMarkParams = reactive<TradeMark>({
+  tmName: '111',
+  logoUrl: '',
+})
+
 // 组件一挂载就发送获取品牌接口请求
 onMounted(() => {
   getHasTrademark()
@@ -83,11 +141,51 @@ const changePageNo = () => {
   getHasTrademark()
 }
 
-//
+// 分页展示，一次展示多少条数据
 const sizeChange = () => {
   pageNo.value = 1
   getHasTrademark()
 }
+
+// 添加品牌
+const addTradeMark = () => {
+  dialogFormVisible.value = true
+}
+
+// 修改品牌
+const updateTradeMark = () => {
+  dialogFormVisible.value = true
+}
+// 删除品牌
+const deleteTradeMark = () => {
+  dialogFormVisible.value = true
+}
+
+// 取消
+const cancle = () => {
+  dialogFormVisible.value = false
+}
+// 提交
+const submit = () => {
+  dialogFormVisible.value = false
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.avatar-uploader {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+</style>

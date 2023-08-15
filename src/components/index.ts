d6a2a18f15204f2b0b3630e15dcf048a3d76f737
slince-zero <1711257704@qq.com>
2023-08-15 14:03:37 +0800

    feat:属性管理静态

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,12 @@
 // import SvgIcon from './SvgIcon/index.vue';
-import SvgIcon from '@/components/SvgIcon/index.vue';
-
-import type { App, Component } from 'vue';
-const components: { [name: string]: Component } = { SvgIcon };
+import SvgIcon from '@/components/SvgIcon/index.vue'
+import Category from '@/components/Category/index.vue'
+import type { App, Component } from 'vue'
+const components: { [name: string]: Component } = { SvgIcon, Category }
 export default {
-    install(app: App) {
-        Object.keys(components).forEach((key: string) => {
-            app.component(key, components[key]);
-        })
-    }
+  install(app: App) {
+    Object.keys(components).forEach((key: string) => {
+      app.component(key, components[key])
+    })
+  },
 }

1a968c0cc247f8547353d57dd7c0ef8429139cdc
slince-zero <1711257704@qq.com>
2023-08-17 10:31:57 +0800

    feat:属性管理-添加

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -95,22 +95,43 @@
       <el-card style="margin: 10px 0">
         <el-form :inline="true">
           <el-form-item label="属性名称">
-            <el-input palceholder="请输入属性名称"></el-input>
+            <el-input
+              palceholder="请输入属性名称"
+              v-model="attrParams.attrName"
+            ></el-input>
           </el-form-item>
         </el-form>
-        <el-button type="primary" icon="Plus">添加属性值</el-button>
+        <el-button
+          :disabled="attrParams.attrName ? false : true"
+          type="primary"
+          icon="Plus"
+          @click="addAttrValue"
+        >
+          添加属性值
+        </el-button>
         <el-button @click="cancle">取消</el-button>
-        <el-table border style="margin: 10px 0; text-align: center">
+        <el-table
+          border
+          style="margin: 10px 0; text-align: center"
+          :data="attrParams.attrValueList"
+        >
           <el-table-column
             label="序号"
             width="80px"
             type="index"
             align="center"
           ></el-table-column>
-          <el-table-column label="属性值名称"></el-table-column>
+          <el-table-column label="属性值名称">
+            <template #="{ row, $index }">
+              <el-input
+                placeholder="请输入属性值名称"
+                v-model="row.valueName"
+              ></el-input>
+            </template>
+          </el-table-column>
           <el-table-column label="属性值操作"></el-table-column>
         </el-table>
-        <el-button type="primary">保存</el-button>
+        <el-button type="primary" @click="save">保存</el-button>
         <el-button @click="cancle">取消</el-button>
       </el-card>
     </div>
@@ -118,10 +139,11 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, watch, ref } from 'vue'
+import { onMounted, watch, ref, reactive } from 'vue'
 // 引入商品分类管理的小仓库
 import useCategoryStore from '@/store/modules/category'
-import { reqAttr } from '@/api/product/attr'
+import { reqAttr, reqAddAttr } from '@/api/product/attr'
+import { ElMessage } from 'element-plus'
 const categoryStore = useCategoryStore()
 onMounted(() => {
   getC1()
@@ -146,22 +168,39 @@ const handler2 = () => {
 
 // 定义数组来存储分类列表
 const attrArr = ref([])
+const getAttr = async () => {
+  const { c1Id, c2Id, c3Id } = categoryStore
+  const result = await reqAttr(c1Id, c2Id, c3Id)
+  if (result.code == 200) {
+    attrArr.value = result.data
+  }
+}
 // 监听三级分类ID，如果存在则发请求
 watch(
   () => categoryStore.c3Id,
   async () => {
     attrArr.value = []
     if (!categoryStore.c3Id) return
-    const { c1Id, c2Id, c3Id } = categoryStore
-    const result = await reqAttr(c1Id, c2Id, c3Id)
-    if (result.code == 200) {
-      attrArr.value = result.data
-    }
+    getAttr()
   },
 )
 
+// 收集新增的属性的数据
+const attrParams = reactive({
+  attrName: '', // 新增属性名称
+  attrValueList: [],
+  categoryId: '', // 三级分类Id
+  categoryLevel: 3,
+})
+
 const scene = ref<number>(0)
 const addAttrArr = () => {
+  Object.assign(attrParams, {
+    attrName: '', 
+    attrValueList: [],
+    categoryId: categoryStore.c3Id, 
+    categoryLevel: 3,
+  })
   scene.value = 1
 }
 const updateAttrArr = () => {
@@ -169,6 +208,30 @@ const updateAttrArr = () => {
 }
 const cancle = () => {
   scene.value = 0
+}
+
+// 添加属性值对象
+const addAttrValue = () => {
+  attrParams.attrValueList.push({
+    valueName: '',
+  })
+}
+
+const save = async () => {
+  const result = await reqAddAttr(attrParams)
+  if (result.code == 200) {
+    scene.value = 0
+    ElMessage({
+      type: 'success',
+      message: '添加成功',
+    })
+    getAttr()
+  } else {
+    ElMessage({
+      type: 'error',
+      message: '添加失败',
+    })
+  }
 }
 </script>
 

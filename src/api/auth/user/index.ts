c39366d1f68aa7abb2707554b09dbcc1aff60614
slince-zero <1711257704@qq.com>
2023-08-17 17:22:07 +0800

    feat:用户管理-添加与修改

---
+++ src/api/auth/user/index.ts
@@ -0,0 +1,32 @@
+import request from "@/utils/request";
+
+enum API {
+    // 用户列表
+    ALLUSER_URL = 'http://114.115.179.162:8022/prod-api/admin/acl/user/',
+    // 添加用户
+    ADDUSER_URL = 'http://114.115.179.162:8022/prod-api/admin/acl/user/save/',
+    // 更新用户
+    UPDATEUSER_URL = 'http://114.115.179.162:8022/prod-api/admin/acl/user/update/',
+    // 角色列表
+    AllRole_URL = 'http://114.115.179.162:8022/prod-api/admin/acl/user/toAssign/',
+    // 更新角色
+    UpdateRole_URL = 'http://114.115.179.162:8022/prod-api/admin/acl/user/doAssignRole',
+    // 删除一个用户
+    DeleteOneUser_URL = 'http://114.115.179.162:8022/prod-api/admin/acl/user/remove/',
+    // 删除批量用户
+    DeleteManyUser_URL = 'http://114.115.179.162:8022/prod-api/admin/acl/user/batchRemove',
+}
+
+export const reqUser = (page: number, limit: number) => request.get<any, any>(API.ALLUSER_URL + `${page}/${limit}`)
+export const reqAddOrUpdateUser = (data: any) => {
+    if (data.id) {
+        return request.put<any, any>(API.UPDATEUSER_URL, data)
+    } else {
+        return request.post<any, any>(API.ADDUSER_URL, data)
+    }
+}
+export const reqRoleList = (userId) => request.get<any, any>(API.AllRole_URL + userId)
+export const reqUpdateRole = (data) => request.post<any, any>(API.UpdateRole_URL, data)
+export const reqDeleteOneUser = (userId) => request.delete<any, any>(API.DeleteOneUser_URL + userId)
+export const reqDeleteManyUser = (idList) => request.delete<any, any>(API.DeleteManyUser_URL, {data: idList})
+
--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -12,24 +12,61 @@
       </el-form>
     </el-card>
     <el-card style="margin: 10px 0">
-      <el-button type="primary">添加用户</el-button>
+      <el-button type="primary" @click="addUser">添加用户</el-button>
       <el-button>批量删除</el-button>
       <div>
         <!-- 展示用户信息 -->
-        <el-table style="margin: 10px 0" border>
+        <el-table style="margin: 10px 0" border :data="userArr">
           <el-table-column type="selection"></el-table-column>
-          <el-table-column label="#" align="center"></el-table-column>
-          <el-table-column label="ID" align="center"></el-table-column>
-          <el-table-column label="用户名字" align="center"></el-table-column>
-          <el-table-column label="用户名称" align="center"></el-table-column>
-          <el-table-column label="用户角色" align="center"></el-table-column>
-          <el-table-column label="创建时间" align="center"></el-table-column>
-          <el-table-column label="更新时间" align="center"></el-table-column>
           <el-table-column
-            label="操作"
+            label="#"
             align="center"
-            width="200px"
+            type="index"
           ></el-table-column>
+          <el-table-column
+            label="ID"
+            align="center"
+            prop="id"
+          ></el-table-column>
+          <el-table-column
+            label="用户名字"
+            align="center"
+            prop="username"
+            show-overflow-tooltip
+          ></el-table-column>
+          <el-table-column
+            label="用户名称"
+            align="center"
+            prop="name"
+            show-overflow-tooltip
+          ></el-table-column>
+          <el-table-column
+            label="用户角色"
+            align="center"
+            prop="roleName"
+            show-overflow-tooltip
+          ></el-table-column>
+          <el-table-column
+            label="创建时间"
+            align="center"
+            prop="createTime"
+            show-overflow-tooltip
+          ></el-table-column>
+          <el-table-column
+            label="更新时间"
+            align="center"
+            prop="updateTime"
+            show-overflow-tooltip
+          ></el-table-column>
+          <el-table-column label="操作" align="center" width="400px">
+            <template #="{ row }">
+              <el-button type="primary" icon="User">分类角色</el-button>
+              <el-button type="primary" icon="Edit" @click="updateUser(row)">
+                编辑角色
+              </el-button>
+              <el-button type="primary" icon="Delete">删除角色</el-button>
+            </template>
+          </el-table-column>
         </el-table>
       </div>
       <div class="pagination">
@@ -38,22 +75,128 @@
           v-model:current-page="pageNow"
           v-model:page-size="pageNum"
           layout="prev, pager, next, jumper"
-          :total="1000"
+          :total="total"
+          @current-change="getHasUser"
           background
         />
       </div>
     </el-card>
+    <!-- 添加和编辑用户信息-抽屉 -->
+    <el-drawer v-model="drawer">
+      <!-- 标题 -->
+      <template #header>
+        <h4>{{ userParams.id ? '编辑用户' : '添加用户' }}</h4>
+      </template>
+      <template #default>
+        <el-form>
+          <el-form-item label="用户姓名">
+            <el-input
+              placeholder="请输入用户姓名"
+              v-model="userParams.username"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="用户昵称">
+            <el-input
+              placeholder="请输入用户昵称"
+              v-model="userParams.name"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="用户密码" v-if="!userParams.id">
+            <el-input
+              placeholder="请输入用户密码"
+              v-model="userParams.password"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+      </template>
+      <template #footer>
+        <div style="flex: auto">
+          <el-button @click="cancel">取消</el-button>
+          <el-button type="primary" @click="save">确认</el-button>
+        </div>
+      </template>
+    </el-drawer>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-
+import { ref, onMounted, reactive } from 'vue'
+import { reqUser, reqAddOrUpdateUser } from '@/api/auth/user'
+import { ElMessage } from 'element-plus'
 // 当前页
 let pageNow = ref(1)
 
 // 一页有几条数据
 let pageNum = ref(5)
+
+// 用户总个数
+const total = ref(0)
+// 存储用户数据的数组
+const userArr = ref([])
+onMounted(() => {
+  getHasUser()
+})
+
+// 定义控制抽屉是否打开变量
+const drawer = ref(false)
+
+// 获取用户管理的信息
+const getHasUser = async (pager = 1) => {
+  // 收集当前页码
+  pageNow.value = pager
+  const result = await reqUser(pageNow.value, pageNum.value)
+  if (result.code == 200) {
+    total.value = result.data.total
+    userArr.value = result.data.records
+  }
+}
+
+// 收集用户的响应式信息
+const userParams = reactive({
+  username: '',
+  name: '',
+  password: '',
+})
+// 添加用户-按钮
+const addUser = () => {
+  // 抽屉显示
+  drawer.value = true
+  Object.assign(userParams, {
+    id: 0,
+    username: '',
+    name: '',
+    password: '',
+  })
+}
+// 编辑用户-按钮
+const updateUser = (row: any) => {
+  // 抽屉显示
+  drawer.value = true
+  Object.assign(userParams, row)
+}
+
+// 取消按钮
+const cancel = () => {
+  drawer.value = false
+}
+const save = async () => {
+  const result = await reqAddOrUpdateUser(userParams)
+  if (result.code == 200) {
+    drawer.value = false
+    ElMessage({
+      type: 'success',
+      message: userParams.id ? '编辑成功' : '添加成功',
+    })
+    getHasUser(userParams.id ? pageNow.value : 1)
+    window.location.reload()
+  } else {
+    drawer.value = false
+    ElMessage({
+      type: 'error',
+      message: userParams.id ? '编辑失败' : '添加失败',
+    })
+  }
+}
 </script>
 
 <style lang="scss" scoped>
